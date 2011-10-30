f817f886e9b4591016e5a08b920b69420a2fa64e
vitroid <vitroid@gmail.com>
2011-10-31 00:51:54 +0900

    Shell scripts and examples are added.

---
+++ work/60x12x1234.sh
@@ -0,0 +1,2 @@
+time ../search2 --depth=12 --compose=60 --bidir --noenergy 1234 < /dev/null | python ../uniquepaths.py 12
+
---
+++ work/60x16x1234.sh
@@ -0,0 +1,2 @@
+time ../search2 --depth=16 --compose=60 --bidir --noenergy 1234 < /dev/null | python ../uniquepaths.py 16
+
